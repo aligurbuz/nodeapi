7692f2405c39913934ca5419fb20d86e19e1d84d
aligurbuz <galiant781@gmail.com>
2016-10-26 12:19:55 +0000

    query builder

--- app/api/node/v1/blog/model/index.js
+++ app/api/node/v1/blog/model/index.js
@@ -10,6 +10,7 @@ module.exports = {
   user: function (callback) {
 
     //query builder for sequelize --select model
+    //select() or where() orderby() offset() groupBy()
     query.table("user").join({model:['task']}).get(function(result) {
       callback(result);
     })
