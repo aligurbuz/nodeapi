19b99aace614433ebe41fed2fb4a6ce0d7b106a6
aligurbuz <galiant781@gmail.com>
2016-11-01 14:46:09 +0000

    token api

--- config/token.js
+++ config/token.js
@@ -1,12 +1,49 @@
 /**
- * get token arrays
+ * token function for all services
+ * Function base module ; api service
+ * token methods are condition for access to service
+ * Examples:
  *
- * @param {object} req
- * @public config.js
+ *     // token check
+ *     //return : token for client
+ *
+ *
+ * @param {String|Array} types...
+ * @return {json}
+ * @return
  */
 
-var token = module.exports = [
+module.exports = {
 
-  'testuser'
+  /**
+   * base ip method for all services
+   * Function base module ; api ip service
+   * get client ip for your services
+   * Examples:
+   *
+   *     // base.ip()
+   *     //return : client ip
+   *
+   *
+   * @param {String|Array} types...
+   * @return {json}
+   * @return
+   */
 
-];
+  TokenForUsers: function () {
+
+    /**
+     * tokens for all services
+     * @type : array
+    */
+    var tokens=[
+      'testuser'
+    ];
+
+    /**
+     * return tokens
+    */
+    return tokens;
+
+  }
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,48 @@
+/**
+ * middleware auth for all services
+ * Function base module ; api service
+ * auth methods are condition for access to service
+ * Examples:
+ *
+ *     // token check
+ *     //return : token for client
+ *
+ *
+ * @param {String|Array} types...
+ * @return {json}
+ * @return
+ */
+
 //in array module
 var in_array = require('in_array');
+
+//load tokens for all services
 var configtoken=require(""+appDir+"/config/token");
 
 // exports function
 module.exports =
 {
-
   get: function (req)
   {
-    //get token
+    /**
+     * req params
+     * get token from url data
+    */
     var token=req.query._token;
 
-    //token control
-    if(in_array(token,configtoken))
-    {
-      //true
-      return true;
-    }
+    //get TokenForUsers function from token
+    var userTokens=configtoken.TokenForUsers();
 
-    //false
+    /**
+     * in_array module
+     * check token from url data
+    */
+    if(in_array(token,userTokens)){
+        return true;
+    }
     return false;
+
+
+
   }
 };
